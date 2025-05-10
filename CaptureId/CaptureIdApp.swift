60ad9c6a0cacbcf7ff158ca9f7268e06f8c45681
Leandro <modena@outlook.com>
2025-05-10 17:18:59 -0300

    Initial Commit

---
+++ CaptureId/CaptureIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureIdApp.swift
+//  CaptureId
+//
+//  Created by Leandro Modena on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptureIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureIdTests/CaptureIdTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureIdTests.swift
+//  CaptureIdTests
+//
+//  Created by Leandro Modena on 10/05/25.
+//
+
+import Testing
+@testable import CaptureId
+
+struct CaptureIdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
